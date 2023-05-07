b63251acd62dcfb2b99b4a89d3d3a1603e98cd0f
Sophia Lin <77797963+slin02@users.noreply.github.com>
2023-05-07 14:53:12 -0400

    more html, added functions in scripts, added css

--- public/scripts.js
+++ public/scripts.js
@@ -1,52 +1,103 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
-// This function shows and hides the shot selection in the interface based 
-// on whether or not the #opponent checkbox is checked
-function showHideShots() {
-    // Get the info from the checkbox
-          let check = document.getElementById('opponent');
-    // Check if the checkbox is checked and show or hide options accordingly
-        if (check.checked == true) {
-    // Here, instead of just showing all of the options, use similar logic to 
-    // check which of the game radio buttons is checked and show only those
-    // options relevant to the game being selected (rps or rpsls). You can 
-    // use similar jQuery 
-            $('.shots').show()
+
+ var rps = true;
+ var opponent = false;
+
+ function playRPS() {
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var spock = document.getElementsByClassName("rpsls")[1];
+    var rps_button = document.getElementsByClassName("rps-button")[0];
+    var rpsls_button = document.getElementsByClassName("rpsls-button")[0];
+    lizard.style.display="none";
+    spock.style.display="none";
+    rps_button.style.opacity="100%";
+    rpsls_button.style.opacity="50%";
+    rps=true;
+ };
+
+function playRPSLS() {
+    var lizard = document.getElementsByClassName("rpsls")[0];
+    var spock = document.getElementsByClassName("rpsls")[1];
+    var rps_button = document.getElementsByClassName("rps-button")[0];
+    var rpsls_button = document.getElementsByClassName("rpsls-button")[0];
+    lizard.style.display="inline-block";
+    spock.style.display="inline-block";
+    rps_button.style.opacity="50%";
+    rpsls_button.style.opacity="100%";
+    rps=false;
+};
+
+function setChoice() {
+    var game=document.getElementsByClassName("dropdown")[0];
+    if (game.style.display=="inline-block") {
+        game.style.display="none";
+        opponent = false;
+    } else {
+        game.style.display="inline-block";
+        opponent = true;
+    }
+}
+
+function openRules() {
+    var rules = document.getElementsByClassName("rules")[0];
+    rules.style.display="inline-block";
+}
+
+function closeRoles() {
+    var rules = document.getElementsByClassName("rules")[0];
+    rules.style.display="none";
+}
+
+function reset() {
+    location.reload();
+}
+
+function closeResults() {
+    var pop_up = document.getElementsByClassName("results")[0];
+    pop_up.style.display="none";
+}
+
+async function play() {
+    var e = document.getElementsById("dropdown")
+    var shot = e.value;
+    var player = document.getElementById("player");
+    var opponent = document.getElementById("opponent");
+    var result = document.getElementById("result");
+    var pop_up = document.getElementsByClassName("results-pop-up")[0];
+
+    if(rps) {
+        if (opponent && shot) {
+            const response = await fetch(`/app/rps/play/${shot}`);
+            const data = await response.json();
+
+            player.innerHTML = `Player: ${data.player}.`
+            opponent.innerHTML = `Opponent: ${data.opponent}.`
+            result.innerHTML = `Result: ${data.result}.`
+            pop_up.style.display = "inline"
+            console.log(data);
         } else {
-            $('.shots').hide()
+            const response = await fetch(`app/rps/play`);
+            const data = await response.json();
+            console.log(data);
+        }
+    } else {
+        if (opponent && shot) {
+            const response = await fetch(`app/rpsls/play/${shot}`);
+            const data = await response.json();
+            console.log(data);
+        } else {
+            const response = await fetch(`app/rpsls/play`)
+            const data = await response.json();
+            console.log(data);
         }
     }
-    // This function clears the input form and also resets the shot selection
-    // radio buttons. 
-    function startOver () {
-        document.getElementById('userinput').reset();
-        showHideShots();
-    }
-    
-    async function playGame () {
-        // Get which game is being played based on the value in the form
-        let game = $('input[type=radio][name=game]:checked').val();
-        // Get which shot is being played based on the value in the form
-        let shot = $('input[type=radio][name=shot]:checked').val();
-        // Identify the base URL based on browser information
-        let baseurl = window.location.href + 'app/'
-        // Log the base URL
-        console.log(baseurl)
-        // This constructs a URL for the opponent option ONLY. To incorporate
-        // the other option, you can use a conditional to change the URL based
-        // on what is selected. You could also write separate functions, or use
-        // a conditional somewhere above in this function to construct the 
-        // correct URL
-        let url = baseurl + game + '/play/' + shot
-        // Log the full URL
-        console.log(url)	
-    
-        let response = await fetch(url)
-        let result = await response.json()
-        // Log the result
-        console.log(result)
+}
+
+// This function shows and hides the shot selection in the interface based 
+// on whether or not the #opponent checkbox is checked
+
         // Here you should include code that uses the DOM API or jQuery to 
         // manipulate another block of HTML in the interface to display the 
         // results in some way. 
-    }
